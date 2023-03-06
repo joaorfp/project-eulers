4beb5b6687faa367a8d4b318c983fdea439e4df4
joaorfp <joaorfp12345@gmail.com>
2023-03-05 22:04:07 -0300

    correção ticker.py e test_ticker.py

--- project/ticker.py
+++ project/ticker.py
@@ -1,34 +1,69 @@
 import pandas_market_calendars as mcal
 import pandas as pd
+from datetime import datetime, timedelta
+from dateutil.relativedelta import *
 
 class Ticker:
     def __init__(self, data=None):
         if data is None:
-            tz = mcal.get_calendar('BVMF').tz.zone
+            tz = mcal.get_calendar('B3').tz.zone
             self.data = pd.Timestamp.now(tz=tz).date()
         else:
             self.data = pd.to_datetime(data).date()
-        
-    def next_expire(self, n=1):
-        calendar = mcal.get_calendar('BVMF')
-        schedule = calendar.schedule(start_date=self.data, end_date=self.data + pd.DateOffset(years=1))
-        expirations = schedule['market_close'].sort_values()
-        print(type(expirations.iloc[n-1]))
-        return expirations.iloc[n-1]
-    
-    def days_until_expire(self, n=1):
-        expire = self.next_expire(n)
-        calendar = mcal.get_calendar('BVMF')
-        schedule = calendar.schedule(start_date=self.data, end_date=expire)
-        util_days = len(schedule)
-        print(util_days)
-    
-    def ticker_days_until_expire(self, ticker):
-        calendar = mcal.get_calendar(ticker)
-        schedule = calendar.schedule(start_date=self.data, end_date=self.next_expire())
-        util_days = len(schedule)
-        return util_days
 
-ticker = Ticker()
-exp = ticker.next_expire(10)
-print(exp)
+    def next_data_expire(self, data):
+        day_15 = data + relativedelta(day=15)
+
+        if int(data.month) % 2 == 0: 
+            weekday = day_15.weekday()
+
+            previous_wednesday = weekday - 2 if weekday >= 2 else weekday + 4
+            next_wednesday = 2 - weekday if weekday <= 2 else 9 - weekday
+
+            if previous_wednesday <= next_wednesday:
+                nearest_wednesday = day_15 - timedelta(days=previous_wednesday)
+            else:
+                nearest_wednesday = day_15 + timedelta(days=next_wednesday)
+
+            return nearest_wednesday
+        else:
+            data += relativedelta(month=int(data.month) + 1)
+            day_15 = data + relativedelta(day=15)
+            weekday = day_15.weekday()
+
+            previous_wednesday = weekday - 2 if weekday >= 2 else weekday + 4
+            next_wednesday = 2 - weekday if weekday <= 2 else 9 - weekday
+
+            if previous_wednesday <= next_wednesday:
+                nearest_wednesday = day_15 - timedelta(days=previous_wednesday)
+            else:
+                nearest_wednesday = day_15 + timedelta(days=next_wednesday)
+
+            return nearest_wednesday
+
+
+    def nth_ticket_expiration(self, ticker):
+            new_data = datetime.now().date()
+            new_data += relativedelta(month=int(new_data.month) + (ticker * 2))
+            return new_data
+
+
+    def next_ticket_expiration(self, ticker=None):
+        b3 = mcal.get_calendar('B3')
+        if ticker:
+            nth_ticket = Ticker.nth_ticket_expiration(self, ticker)
+            next_ticket_to_expire = Ticker.next_data_expire(self, nth_ticket)
+            datas = b3.schedule(start_date=datetime.now().date(), end_date=next_ticket_to_expire)
+            
+            print(f'Ticket will expire at {next_ticket_to_expire}. {len(datas)} days until the ticker n{ticker} from now to expire')
+            return f'Ticket will expire at {next_ticket_to_expire}. {len(datas)} days until the ticker n{ticker} from now to expire'
+
+        else:
+            next_ticket_to_expire = Ticker.next_data_expire(self, self.data)
+            datas = b3.schedule(start_date=datetime.now().date(), end_date=next_ticket_to_expire)
+            
+            print(f'Ticket will expire at {next_ticket_to_expire}. Util-Days to ticket expiration: {len(datas)}')
+            return f'Ticket will expire at {next_ticket_to_expire}. Util-Days to ticket expiration: {len(datas)}'
+
+# ticker = Ticker()
+# ticker.next_ticket_expiration()
--- tests/test_ticker.py
+++ tests/test_ticker.py
@@ -1,24 +1,40 @@
-from datetime import date
+from datetime import datetime, date
 from project.ticker import Ticker
 import pandas as pd
+import unittest
+import pandas_market_calendars as mcal
+import pandas as pd
 
-def test_init_with_no_data():
-    ticker = Ticker()
-    assert ticker.data == date.today()
+class Tests(unittest.TestCase):
+    def setUp(self):
+        self.date = date.today()
 
+    def test_next_data_expire(self):
+        ticker = Ticker()
+        assert 1 == 1
+        mock_date = datetime.strptime('2023-04-12', '%Y-%m-%d').date()
+        assert ticker.next_data_expire(self.date) == mock_date
 
-def test_init_with_data():
-    date_str = "2022-02-28"
-    ticker = Ticker(data=date_str)
-    assert str(ticker.data) == date_str
+    def test_nth_ticket_expiration(self):
+        ticker = Ticker()
+        mock_date = datetime.strptime('2023-06-14', '%Y-%m-%d').date()
+        date_nth_ticker = ticker.nth_ticket_expiration(1)
+        assert ticker.next_data_expire(date_nth_ticker) == mock_date
 
+        mock_date2 = datetime.strptime('2023-08-16', '%Y-%m-%d').date()
+        date_nth_ticker = ticker.nth_ticket_expiration(2)
+        assert ticker.next_data_expire(date_nth_ticker) == mock_date2
 
-def test_next_expire():
-    ticker = Ticker()
-    assert ticker.next_expire(1) == pd.Timestamp('2023-02-28 21:00:00+0000', tz='UTC')
+    def test_next_ticket_expiration(self):
+        b3 = mcal.get_calendar('B3')
+        param = 1
+        ticker = Ticker()
+        next_ticket_to_expire = ticker.next_data_expire(self.date)
+        datas = b3.schedule(start_date=datetime.now().date(), end_date=next_ticket_to_expire)
+        assert ticker.next_ticket_expiration() == f'Ticket will expire at {next_ticket_to_expire}. Util-Days to ticket expiration: {len(datas)}'
 
-
-def test_ticker_days_until_expire():
-    ticker = Ticker()
-    assert ticker.ticker_days_until_expire('BVMF') == 1
-
+        """Test nth ticket"""
+        date_nth_ticker = ticker.nth_ticket_expiration(param)
+        next_ticket_to_expire = ticker.next_data_expire(date_nth_ticker)
+        datas = b3.schedule(start_date=datetime.now().date(), end_date=next_ticket_to_expire)
+        assert ticker.next_ticket_expiration(param) == f'Ticket will expire at {next_ticket_to_expire}. {len(datas)} days until the ticker n{param} from now to expire'
